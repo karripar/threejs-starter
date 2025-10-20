2b20d22cf75af8d052f479ed578a251ec062f6b4
karripar <karripartanen25@gmail.com>
2025-10-20 15:42:17 +0300

    added a nice line of code

--- src/main.js
+++ src/main.js
@@ -1,27 +1,60 @@
-import './style.css'
-import * as THREE from 'three'
+import './style.css';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+let scene, camera, renderer, cube;
 
-const renderer = new THREE.WebGLRenderer();
+const init = () => {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+  renderer = new THREE.WebGLRenderer();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshPhongMaterial({color: 0x00ff00});
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  scene.add(directionalLight);
+  camera.position.z = 5;
+};
+
+init();
 
 const animate = () => {
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-}
-renderer.setAnimationLoop( animate );
+};
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+renderer.setAnimationLoop(animate);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 2;
+controls.maxDistance = 10;
+controls.maxPolarAngle = Math.PI / 2;
